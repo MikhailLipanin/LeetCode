79070cdb3c06b6bd95379de3a7d19fdf803c4980
Mikhail Lipanin <95661085+MikhailLipanin@users.noreply.github.com>
2024-01-16 14:45:40 +0300

    Time: 0 ms (100.00%), Space: 2.4 MB (82.35%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.go
@@ -0,0 +1,36 @@
+func gcdOfStrings(str1 string, str2 string) string {
+    n, m := len(str1), len(str2)
+    if n > m {
+        n, m = m, n
+        str1, str2 = str2, str1
+    }
+    maxPrefix := 0
+    for i := 0; i < n; i++ {
+        if m % (i+1) != 0 || n % (i+1) != 0 {
+            continue
+        }
+        tmp := str1[:i+1]
+        ok := true
+        for j := 0; j < m; j++ {
+            if str2[j] != tmp[j%(i+1)] {
+                ok = false
+                break
+            }
+            if j < n && str1[j] != tmp[j%(i+1)] {
+                ok = false
+                break
+            }
+        }
+        if ok {
+            maxPrefix = i+1
+        }
+    }
+    return str1[:maxPrefix]
+}
+
+func min(x, y int) int {
+    if x < y {
+        return x
+    }
+    return y
+}
